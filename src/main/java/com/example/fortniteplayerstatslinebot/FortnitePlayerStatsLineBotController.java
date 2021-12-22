f253cbed596233078e7a34e11d738846c01b033a
otkshol <otkshol@gmail.com>
2021-12-22 09:46:41 +0900

    first commit

---
+++ src/main/java/com/example/fortniteplayerstatslinebot/FortnitePlayerStatsLineBotController.java
@@ -0,0 +1,116 @@
+package com.example.fortniteplayerstatslinebot;
+
+
+import com.linecorp.bot.client.LineBlobClient;
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.client.MessageContentResponse;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.action.DatetimePickerAction;
+import com.linecorp.bot.model.action.MessageAction;
+import com.linecorp.bot.model.action.PostbackAction;
+import com.linecorp.bot.model.action.URIAction;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.StickerMessageContent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.source.GroupSource;
+import com.linecorp.bot.model.event.source.RoomSource;
+import com.linecorp.bot.model.event.source.Source;
+import com.linecorp.bot.model.group.GroupMemberCountResponse;
+import com.linecorp.bot.model.group.GroupSummaryResponse;
+import com.linecorp.bot.model.message.*;
+import com.linecorp.bot.model.message.imagemap.*;
+import com.linecorp.bot.model.message.sender.Sender;
+import com.linecorp.bot.model.message.template.*;
+import com.linecorp.bot.model.response.BotApiResponse;
+import com.linecorp.bot.model.room.RoomMemberCountResponse;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
+
+import static java.util.Collections.singletonList;
+
+@Slf4j
+@LineMessageHandler
+public class FortnitePlayerStatsLineBotController {
+
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
+    @EventMapping
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+
+        // TODO LINEのユーザーIDを取得して一般用とプライベート用の振る舞いを分岐させる（外に対象ID一覧を置いておいて、Streamを使用して絞り込む。）
+        // 一般用から着手
+        TextMessageContent message = event.getMessage();
+        handleTextContent(event.getReplyToken(), event, message);
+    }
+
+    // サンプルから持ってきたのでここから修正する
+    private void handleTextContent(String replyToken, Event event, TextMessageContent content)
+            throws Exception {
+        final String text = content.getText();
+        log.info("Got text message from replyToken:{}: text:{} emojis:{}", replyToken, text,
+                content.getEmojis());
+        switch (text) {
+            case "ビクロイ": {
+                lineMessagingClient.getBotInfo();
+                this.replyText(replyToken, "ビクロイって入力しましたね。APIをたたいて結果取得する予定です");
+            }
+            case "キルレート": {
+                lineMessagingClient.getBotInfo();
+                this.replyText(replyToken, "キルレートって入力しましたね。APIをたたいて結果取得する予定です");
+            }
+
+            default:
+                log.info("Returns echo message {}: {}", replyToken, text);
+                this.replyText(
+                        replyToken,
+                        text
+                );
+                break;
+        }
+    }
+
+    private void replyText(@NonNull String replyToken, @NonNull String message) {
+        if (replyToken.isEmpty()) {
+            throw new IllegalArgumentException("replyToken must not be empty");
+        }
+        if (message.length() > 1000) {
+            message = message.substring(0, 1000 - 2) + "……";
+        }
+        this.reply(replyToken, new TextMessage(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull Message message) {
+        reply(replyToken, singletonList(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+        reply(replyToken, messages, false);
+    }
+
+    private void reply(@NonNull String replyToken,
+                       @NonNull List<Message> messages,
+                       boolean notificationDisabled) {
+        try {
+            BotApiResponse apiResponse = lineMessagingClient
+                    .replyMessage(new ReplyMessage(replyToken, messages, notificationDisabled))
+                    .get();
+            log.info("Sent messages: {}", apiResponse);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
