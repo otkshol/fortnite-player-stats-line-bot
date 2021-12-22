2faa9ed16ad0baa94bbabe7dd81d71c2369b4272
otkshol <otkshol@gmail.com>
2021-12-22 17:29:46 +0900

    chore: fix build.gradle

--- src/main/java/com/fortniteplayerstatslinebot/FortnitePlayerStatsLineBotApplication.java
+++ src/main/java/com/fortniteplayerstatslinebot/FortnitePlayerStatsLineBotApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FortnitePlayerStatsLineBotApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FortnitePlayerStatsLineBotApplication.class, args);
     }
-
 }
